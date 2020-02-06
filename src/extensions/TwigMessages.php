6fa40955ced0c256d8c8f75fd6e2f5547afa1e5a
Jules Sayer <jules.sayer@protonmail.com>
2020-02-06 23:02:21 +0100

    Ajout Extensions twig ;)
    Signed-off-by: Jules Sayer <jules.sayer@protonmail.com>

---
+++ src/extensions/TwigMessages.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace src\extensions;
+
+use Slim\Flash\Messages;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+/**
+ * Class TwigMessages
+ * Adapaté car outdated sur ce repo
+ *
+ * @author Jules Sayer <jules.sayer@protonmail.com>
+ * @link https://github.com/kanellov/slim-twig-flash for the canonical source repository
+ * @package src\extensions
+ */
+class TwigMessages extends AbstractExtension {
+
+    /**
+     * @var Messages
+     */
+    protected $flash;
+
+    /**
+     * TwigMessages constructor.
+     *
+     * @param Messages $flash
+     */
+    public function __construct(Messages $flash) {
+        $this->flash = $flash;
+    }
+
+    /**
+     * Nom de l'extension
+     *
+     * @return string
+     */
+    public function getName() {
+        return 'slim-twig-flash';
+    }
+
+    /**
+     * Callback pour twig.
+     *
+     * @return array
+     */
+    public function getFunctions() {
+        return [
+            new TwigFunction('flash', [$this, 'getMessages']),
+        ];
+    }
+
+    /**
+     * Retourne les messages flash, si la clé est donnée
+     * en paramètre, alors retourne les messages associés
+     * à la clé
+     *
+     * @param string $key
+     * @return array
+     */
+    public function getMessages($key = null) {
+        if (null !== $key) {
+            return $this->flash->getMessage($key);
+        }
+
+        return $this->flash->getMessages();
+    }
+}
