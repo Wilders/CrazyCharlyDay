62c85210c9bc5aa4b1826ac982cbf708790ea27c
PERNOT Anthony <anthony.pernot@hotmail.fr>
2020-02-13 12:08:54 +0100

    Ajout controleur des créneaux + exception créneaux

---
+++ src/exceptions/NicheException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace src\exceptions;
+
+use Exception;
+
+/**
+ * Class AuthException
+ * @author Anthony PERNOT <anthony.pernot@hotmail.fr>
+ * @package src\exceptions
+ */
+class NicheException extends Exception {}
