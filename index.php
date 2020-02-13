781ad9869b420c2f3880ce9b3fd4b2a898921deb
PERNOT Anthony <anthony.pernot@hotmail.fr>
2020-02-13 13:26:12 +0100

    fonctionnalité ajout créneau (pas testé encore) + affichage complète des créneaux activés + route

--- index.php
+++ index.php
@@ -120,6 +120,7 @@ $app->group('/admin', function (){
     $this->get('/', AdminController::class . ':showAdmin')->setName('showAdmin');
     $this->get('/delete/{id}', AdminController::class .':deleteUser')->setName('deleteUser');
     $this->get('/register', AdminController::class . ':showRegister')->setName('showRegister');
+    $this->get('/niche', NicheController::class . ':showNiche')->setName("showNiche");
     $this->post('/register', AdminController::class . ':register')->setName('register');
 
 })->add(new AdminMiddleware($container));
--- src/controllers/NicheController.php
+++ src/controllers/NicheController.php
@@ -20,4 +20,52 @@ class NicheController extends Controller{
         return $response;
     }
 
+    public function niches(Request $request, Response $response, array $args) : Response {
+        try{
+            $niches = Niche::where('statut','=',1)->get();
+
+            $this->view->render($response, 'pages/niche.twig',[
+                "niches" => $niches
+            ]);
+            return $response;
+
+        }catch (NicheException $e){
+            $this->flash->addMessage('error', $e->getMessage());
+            $response = $response->withRedirect($this->router->pathFor($e->getRoute()));
+        }
+    }
+
+    public function addNiche(Request $request, Response $response, array $args) : Response {
+        try{
+            $day = filter_var($request->getParsedBodyParam('day'), FILTER_SANITIZE_NUMBER_INT);
+            $week = filter_var($request->getParsedBodyParam('week'), FILTER_SANITIZE_SPECIAL_CHARS);
+            $begin = filter_var($request->getParsedBodyParam("begin"), FILTER_SANITIZE_NUMBER_INT);
+            $end = filter_var($request->getParsedBodyParam("end")? FILTER_SANITIZE_NUMBER_INT);
+            $cycle = filter_var($request->getParsedBodyParam("cycle"), FILTER_SANITIZE_NUMBER_INT);
+            $statut = filter_var($request->getParsedBodyParam("statut"), FILTER_SANITIZE_SPECIAL_CHARS);
+
+            if($day != (1 || 2 || 3 || 4 || 5 || 6 || 7)) throw new NicheException("Le jour sélectionnée ne peut pas être choisi")
+            if($week != ("A" || "B" || "C" || "D")) throw new NicheException("La semaine sélectionnée ne peut pas $etre choisi");
+            if (Niche::where(['begin' => $begin, 'end' => $end, 'day' => $day, 'week' => $week, 'cycle_id' => $cycle])->exists()) throw new NicheException("Ce créaneau est déjà pris.");
+
+            $niche = new Niche();
+            $niche->day = $day;
+            $niche->week = $week;
+            $niche->begin = $begin;
+            $niche->end = $end;
+            $niche->cycle_id = $cycle;
+            $niche->statut = $statut;
+            $niche->save();
+
+            $this->flash->addMessage('success', "Votre créneau a bien été enregistré.");
+            $response = $response->withRedirect($this->router->pathFor('showAdmin'));
+
+        }catch (NicheException $e){
+            $this->flash->addMessage('error', "Vous rencontrez un problème pour l'inscription à un créneau, veuillez réessayer ultérieurement.");
+            $response = $response->withRedirect($this->router->pathFor('home'));
+        }
+    }
+
+
+
 }
