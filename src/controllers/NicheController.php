aa7d6f156109276416ccb717dc5d61c173851710
PERNOT Anthony <anthony.pernot@hotmail.fr>
2020-02-13 19:39:02 +0100

    fonctionnalité modification suppression de créneau côté back

--- src/controllers/NicheController.php
+++ src/controllers/NicheController.php
@@ -48,11 +48,63 @@ class NicheController extends Controller{
             $this->flash->addMessage('success', "Votre créneau a bien été enregistré.");
             $response = $response->withRedirect($this->router->pathFor('showAdmin'));
 
-        }catch (NicheException $e){
+        }catch (ModelNotFoundException $e){
             $this->flash->addMessage('error', $e->getMessage());
             $response = $response->withRedirect($this->router->pathFor('formNiche'));
         }
         return $response;
+    }
+
+    public function updateNiche(Request $request, Response $response, array $args) : Response {
+        try{
+            $idNiche = filter_var($request->getParsedBodyParam("idNiche"), FILTER_SANITIZE_NUMBER_INT);
+            $begin = filter_var($request->getParsedBodyParam("begin"), FILTER_SANITIZE_SPECIAL_CHARS);
+            $end = filter_var($request->getParsedBodyParam("end"), FILTER_SANITIZE_NUMBER_INT);
+            $day = filter_var($request->getParsedBodyParam('day'), FILTER_SANITIZE_NUMBER_INT);
+            $week = filter_var($request->getParsedBodyParam('week'), FILTER_SANITIZE_SPECIAL_CHARS);
+            $cycle = filter_var($request->getParsedBodyParam("cycle"), FILTER_SANITIZE_NUMBER_INT);
+
+            is_null($request->getParsedBodyParam("statut"))? $statut = 0 : $statut = 1;
+            if($begin>=$end) throw new NicheException("L'heure de début est supérieure ou égale à l'heure de fin");
+            if($day != (1 || 2 || 3 || 4 || 5 || 6 || 7)) throw new NicheException("Le jour sélectionnée ne peut pas être choisi");
+            if($week != ("A" || "B" || "C" || "D")) throw new NicheException("La semaine sélectionnée ne peut pas etre choisi");
+            if (Niche::where(['begin' => $begin, 'end' => $end, 'day' => $day, 'week' => $week, 'cycle_id' => $cycle])->exists()) throw new NicheException("Ce créaneau est déjà pris.");
+
+            $niche = Niche::where("id","=",$idNiche)->firstOrFail();
+
+            $niche->begin = $begin;
+            $niche->end = $end;
+            $niche->day = $day;
+            $niche->week = $week;
+            $niche->cycle = $cycle;
+
+            $niche->save();
+
+            $this->flash->addMessage('success', "Votre créneau a bien été modifié.");
+            $response = $response->withRedirect($this->router->pathFor('showAdmin'));
+            return $response;
+        }catch(ModelNotFoundException $e){
+            $this->flash->addMessage('error', "Ce créneau n'existe pas.");
+            $response = $response->withRedirect($this->router->pathFor('showAdmin'));
+        }
+    }
+
+    public function deleteNiche(Request $request, Response $response, array $args) : Response {
+        try{
+            $idNiche = filter_var($request->getParsedBodyParam("idNiche"), FILTER_SANITIZE_NUMBER_INT);
+
+            $niche = Niche::where("id","=",$idNiche)->firstOrFail();
+            $need = Need::where("niche_id","=",$niche->id)->firstOrFail();
+            unset($need->niche_id);
+            $niche->destroy();
+
+            $this->flash->addMessage('success', "Votre créneau a bien été supprimé.");
+            $response = $response->withRedirect($this->router->pathFor('showAdmin'));
+            return $response;
+        }catch(ModelNotFoundException $e){
+            $this->flash->addMessage('error', "Ce créneau n'existe pas.");
+            $response = $response->withRedirect($this->router->pathFor('showAdmin'));
+        }
     }
 
     public function formNiche(Request $request, Response $response, array $args) : Response{
